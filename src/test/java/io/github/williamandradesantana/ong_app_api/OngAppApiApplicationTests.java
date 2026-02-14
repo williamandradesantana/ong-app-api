878f5d85adfea7b8727cda96ab331ede320be722
William Santana <williamandrade1058@gmail.com>
2026-02-14 15:26:51 -0300

    start project

---
+++ src/test/java/io/github/williamandradesantana/ong_app_api/OngAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.williamandradesantana.ong_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
