5c61a871f9b9b939b5b33bb9666ca04fd69fbbbc
William Santana <williamandrade1058@gmail.com>
2026-02-16 09:43:13 -0300

    feat(roles): find all roles true enabled

--- src/main/java/io/github/williamandradesantana/ong_app_api/modules/roles/repository/RoleRepository.java
+++ src/main/java/io/github/williamandradesantana/ong_app_api/modules/roles/repository/RoleRepository.java
@@ -2,9 +2,12 @@ package io.github.williamandradesantana.ong_app_api.modules.roles.repository;
 
 
 import io.github.williamandradesantana.ong_app_api.modules.roles.entity.RoleEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
+    Page<RoleEntity> findByEnabledTrue(Pageable pageable);
 }
