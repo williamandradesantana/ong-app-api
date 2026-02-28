73343805818ef8cebbff243dae5315291e9afaf8
William Santana <williamandrade1058@gmail.com>
2026-02-28 18:25:12 -0300

    feat: add CORS configuration to allow frontend requests

--- src/main/java/io/github/williamandradesantana/ong_app_api/config/WebConfiguration.java
+++ src/main/java/io/github/williamandradesantana/ong_app_api/config/WebConfiguration.java
@@ -1,12 +1,17 @@
 package io.github.williamandradesantana.ong_app_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -17,5 +22,14 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("yaml", new MediaType("application", "yaml"));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true);
     }
 }
